c95a19834e4a6a3977f95346b1bbf5a4668c76e5
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2015-09-10 16:28:35 -0700

    Refactoring to use roolz, content-formatter, content-logger, string-sub

---
+++ bin/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var updateNotifier = require('update-notifier');
+
+// var notifier = updateNotifier(
+// 	{
+// 		pkg: require('../package.json')
+// 	}
+// );
+
+// if (notifier.update) {
+// 	notifier.notify();
+// }
+
+require('../lib/cli').init();
