5879c6ade2110958b6f6f5bcf98c664119bffcab
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2015-09-17 13:41:26 -0700

    Removing update notifier

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,3 @@
 #!/usr/bin/env node
 
-var updateNotifier = require('update-notifier');
-
-// var notifier = updateNotifier(
-// 	{
-// 		pkg: require('../package.json')
-// 	}
-// );
-
-// if (notifier.update) {
-// 	notifier.notify();
-// }
-
 require('../lib/cli').init();
